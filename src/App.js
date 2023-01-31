6953390321e630b87b196286f5259da0a05245fe
Sirjamo1 <jam_how@hotmail.com>
2023-01-31 12:19:27 +1000

    getting pages to work

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
     return (
-        <BrowserRouter basename="/checkout-app">
+        <BrowserRouter basename="/">
             <NavBar />
             <Routes>
                 <Route exact path="/" element={<Home />}></Route>
